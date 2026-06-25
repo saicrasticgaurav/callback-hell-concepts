bd4e45cc15b62e2e300ba3f3541031644a252b93
Gaurav patil <gp249780@gmail.com>
2026-06-25 20:39:33 +0530

    promisess chaining

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
-function sevetoDb(data){
-    return new Promise((resolve,reject)=>{
-    let internatespeed = Math.floor(Math.random()*10)+1;
-    if (internatespeed>4){
-        resolve("data saved succesfully"+data);
-    }else{
-        reject("weeak connection");
-    }
+function getlogin(){
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve("user login succfully");
+        },1000);
     });
 }
-sevetoDb("gaurav")
+
+function getprofile(){
+    return new Promise ((resolve)=>{
+        setTimeout(()=>{
+            resolve("user get profile sussfully");
+        },1000);
+    });
+}
+function getcomment(){
+    return new Promise((resolve)=>{
+        setTimeout(() => {
+           resolve("user post comment sussfully"); 
+        },1000);
+    });
+}
+
+getlogin()
 .then((result)=>{
-    console.log("success",result);
+    console.log(result);
+    return getprofile();
+})
+.then((result)=>{
+    console.log(result);
+    return getcomment();
+})
+.then((result)=>{
+    console.log(result);
 })
 .catch((error)=>{
-    console.log("error weak connection form",error)
+    console.log(error);
 });
-console.log("Promise practice completed");
+
